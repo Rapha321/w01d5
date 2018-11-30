55a300346ca5dcac294dcd64b6b84c349bea1de3
Rapha321 <ralpha19@hotmail.com>
2018-11-30 16:35:45 +0000

    added chalk function

---
+++ index.js
@@ -0,0 +1,6 @@
+var chalk = require("chalk");
+
+var message = "Hello " + chalk.blue("Blue World");
+var message1 = "Hello " + chalk.green("Green World");
+console.log(message);
+console.log(message1);
